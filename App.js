775e36444517af534077310be4405ad37dd8fe3f
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2024-03-19 19:13:18 -0500

    Laptop Update
    -->init Counter app.
    -->Basic functionality working.

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  var [counter, setCounter] = useState(0);
+
+  var increaseCounter = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
+  var decreaseCounter = () => {
+    setCounter(counter - 1);
+    console.log(counter);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button style={styles.button} title="--" onPress={decreaseCounter}/>
+      <Text style={styles.counter}>Number: {counter}</Text>
+      <Button style={styles.button} title="++" onPress={increaseCounter}/>
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    color: '#000',
+    fontSize: 30,
+    backgroundColor: '#CB6100',
+  },
+  button: {
+    backgroundColor: '#000',
+    borderRadius: 200,
   },
 });
